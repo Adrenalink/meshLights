91915f6da14e6605d28b24f0f79a7a23ee0cf427
Michael McMillan <michael.mcmillan@sonos.com>
2019-05-03 13:01:21 -0700

    Animation function updates.

--- src/main.cpp
+++ src/main.cpp
@@ -46,9 +46,11 @@ void setupMesh();
 void updateMesh();
 
 // Global vars
-int display_mode = ALONE;   // animation type -- init animation as single node
-bool amController = false;  // flag to designate that this node is the current controller
-uint8_t gHue = 192;         // rotating "base color" used by many of the patterns (0=red, 32=orange, 64=yellow, 128=blue, 192=purple, 255=also red?)
+int display_mode = ALONE;          // animation type -- init animation as single node
+bool amController = false;         // flag to designate that this node is the current controller
+uint16_t gHue = 0;                 // rotating color used to shift the rainbow animation
+uint8_t aloneHue = random(0,254);  // random color set on each reboot, used for the color in the confetti() ("alone") function/animation
+uint8_t animationDelay = random(5,40); // random animation speed, used to create a unique color/vibration scheme for each individual light
 
 painlessMesh mesh;
 
@@ -72,6 +74,14 @@ void sinelon_white() { // slightly different cylon effect, this one a white dot 
   leds[pos] = CRGB::White;
 }
 
+void confetti()
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy(leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV(aloneHue + random8(32), 200, 255); // was random8(64)
+}
+
 void rainbow() {
   fill_rainbow(leds, NUM_LEDS, gHue, 7); // FastLED's built-in rainbow generator
 }
@@ -83,14 +93,16 @@ void addGlitter(fract8 chanceOfGlitter) {
 void stepAnimation(int display_mode) {
   switch (display_mode) {
     case 1: // "cylon" effect, searching for connections
-      sinelon();
+      //sinelon();
+      EVERY_N_MILLISECONDS( animationDelay ) { confetti(); }
+      //confetti();
       FastLED.show();
     break;
 
     case 2: // "rainbow" effect, connected!
       rainbow();
       //addGlitter(10);
-      sinelon_white();
+      //sinelon_white();
       FastLED.show();
     break;
   }
@@ -178,14 +190,9 @@ void changedConnectionCallback() {
     Serial.println();
     amController = true;
 
-    // ON CHANGE, IF THE NODE COUNT IS "0" THEN THE CONTROLLER IS ALONE AND GOES BACK TO THE "SEEKING" ANIMATION
-    if (nodes.size() > 0) {
-      display_mode = CONNECTED;
-      // need clock sync data to be sent here?  I think just leds[pos] needs to be sent... See nodeTimeAdjustedCallback method.
-    }
-    else {
-      display_mode = ALONE;
-    }
+    // AS THE CONTROLLER, IF THE NODE COUNT IS "0" YOU ARE ALONE, GO BACK TO THE "ALONE" ANIMATION
+    if (nodes.size() > 0) { display_mode = CONNECTED; }
+    else { display_mode = ALONE; }
   }
   else {
     Serial.printf("Node %u is the controller now", lowestNodeID);
@@ -220,13 +227,17 @@ void sortNodeList(SimpleList<uint32_t> &nodes) {
   for (SimpleList<uint32_t>::iterator node = nodes_sorted.begin(); node != nodes_sorted.end(); ++node) nodes.push_back(*node);
 } // sortNodeList
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+// BASICS
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 void setup() {
   Serial.begin(115200);
-  setupLEDs(); // creates new LED object
+  setupLEDs(); // creates a new LED object
   setupMesh(); // creates a new mesh network
 }
 
 void loop() {
   updateMesh(); // check connected status and update meshed nodes, checks for controller status and calls stepAnimation()
-  EVERY_N_MILLISECONDS( 20 ) { shiftHue(); } // increment base hue for a shifting rainbow effect
+  EVERY_N_MILLISECONDS( 10 ) { shiftHue(); } // increment base hue for a shifting rainbow effect
 }
